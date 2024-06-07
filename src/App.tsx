004be3b8ba04ce9d535e54947a6d75c76af72799
Camilla Landin <landin.camilla8@gmail.com>
2024-06-07 16:56:21 +0200

    react query tools and client

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,8 @@ import HomePage from './pages/HomePage';
 import '../src/assets/App.scss';
 import Navigation from './components/Navigation';
 import NotFoundPage from './pages/NotFoundPage';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import GenresPage from './pages/GenresPage';
 
 function App() {
 
@@ -16,10 +18,13 @@ function App() {
 			<Container className='pt-2'>
 				<Routes>
 					<Route path="/" element={<HomePage />} />
+					<Route path="/genres" element={<GenresPage />} />
 
 					<Route path="*" element={<NotFoundPage />} />
 				</Routes>
 			</Container>
+
+		<ReactQueryDevtools />
 	</>
   )
 }
--- src/main.tsx
+++ src/main.tsx
@@ -5,10 +5,11 @@ import { BrowserRouter } from 'react-router-dom'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
 const queryClient = new QueryClient({
-  defaultOptions: {
-    queries: {
-      refetchOnWindowFocus: false,
-    },
+	defaultOptions: {
+		queries: {
+			refetchOnWindowFocus: false,
+			staleTime: 1000 * 60 * 5,
+		},
   },
 });
 
