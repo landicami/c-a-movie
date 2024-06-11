d931db0582a9dee6e0e4536f3f1a7acc474bbe7c
Camilla Landin <landin.camilla8@gmail.com>
2024-06-11 14:17:28 +0200

    Better homepage

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,8 +1,24 @@
+
+import { Link } from "react-router-dom";
+
 const HomePage = () => {
 
   return (
     <>
-      <h1>I will see you at home</h1>
+	<div className='p-2 rounded mb-2'>
+		<div className="col-sm-12">
+		<h1>Find ze code for your favorite movie</h1>
+
+		<Link to={"/genres"} className='btn btn-secondary m-1' role='button'>Now playing</Link>
+		<Link to={"/genres"} className='btn btn-secondary m-1' role='button'>Trending</Link>
+		<Link to={"/genres"} className='btn btn-secondary m-1' role='button'>Top Rated</Link>
+		<Link to={"/genres"} className='btn btn-secondary m-1' role='button'>By genre</Link>
+
+
+
+		</div>
+	</div>
+
 
     </>
   );
