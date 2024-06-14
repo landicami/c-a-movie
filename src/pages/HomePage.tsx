9bee5d13ce4f1a65e6d7de1087f1ae57fa67b18d
Camilla Landin <landin.camilla8@gmail.com>
2024-06-14 15:34:12 +0200

    some styling on homepage

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -5,23 +5,19 @@ const HomePage = () => {
 
   return (
     <>
-	<div className="">
-		<div className="">
-			<h1 className="text-center align-items-center">Welcome</h1>
-			<div className='d-flex justify-content-center'>
+	<div className="d-flex justify-content-center rounded border hero">
+		<div className="text-center">
+			<h1 className="m-4">Welcome</h1>
 
-			<div className="p-2 border rounded mb-2 col-lg-6 col-sm-12">
-			<h2>Find ze code for your favorite movie</h2>
+			<div className="p-4 border rounded mb-2 col-lg-12 col-sm-12 cinema-short mb-4">
+			<h2 className="text-center text-white">Find ze code for your favorite movie</h2>
 
 			<Link to={"/nowplaying"} className='btn btn-secondary m-1' role='button'>Now playing</Link>
 			<Link to={"/trending"} className='btn btn-secondary m-1' role='button'>Trending</Link>
 			<Link to={"/toprated"} className='btn btn-secondary m-1' role='button'>Top Rated</Link>
 			<Link to={"/genres"} className='btn btn-secondary m-1' role='button'>By genre</Link>
 
-
-
-		</div>
-		</div>
+			</div>
 
 	</div>
 
