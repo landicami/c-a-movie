f070d447e53b6dd7e391edc145dbcd032d57ae2f
Camilla Landin <landin.camilla8@gmail.com>
2024-06-18 11:45:47 +0200

    added paginationcomponent

---
+++ src/components/Pagination.tsx
@@ -0,0 +1,53 @@
+import React from "react";
+import Button from "react-bootstrap/Button";
+
+interface PaginationProps{
+	hasNextPage: boolean
+	hasPreviousPage: boolean;
+	page: number
+	totalPages: number
+	onNextPage: () => void;
+	onPreviousPage: () => void;
+}
+
+const Pagination: React.FC<PaginationProps> = ({
+	page,
+	totalPages,
+	hasPreviousPage,
+	hasNextPage,
+	onNextPage,
+	onPreviousPage,
+  }) => {
+  return (
+
+		<div className='row'>
+			<div className='d-flex justify-content-center'>
+				<div>
+				<Button
+				variant="secondary"
+				onClick={onPreviousPage}
+				disabled={hasPreviousPage}
+				>
+					&laquo;
+				</Button>
+				</div>
+				<div className='mb-0 p-2 rounded'>
+					<p className='m-0'>
+						{page} of {500}
+					</p>
+				</div>
+				<div>
+					<Button
+					variant="secondary"
+					onClick={onNextPage}
+					disabled={hasNextPage}
+					>
+					&raquo;</Button>
+				</div>
+			</div>
+		</div>
+
+  )
+}
+
+export default Pagination;
