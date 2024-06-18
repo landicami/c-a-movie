1dea3da0f64ce2a8d79c2cf5b80163cf547a1112
Camilla Landin <landin.camilla8@gmail.com>
2024-06-18 11:46:20 +0200

    styling

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -5,12 +5,12 @@ const HomePage = () => {
 
   return (
     <>
-	<div className="d-flex justify-content-center rounded border hero">
+	<div className="d-flex justify-content-center">
 		<div className="text-center">
 			<h1 className="m-4">Welcome</h1>
 
 			<div className="p-4 border rounded mb-2 col-lg-12 col-sm-12 cinema-short mb-4">
-			<h2 className="text-center text-white">Find ze code for your favorite movie</h2>
+			<h2 className="text-center">Find ze code for your favorite movie</h2>
 
 			<Link to={"/nowplaying"} className='btn btn-secondary m-1' role='button'>Now playing</Link>
 			<Link to={"/trending"} className='btn btn-secondary m-1' role='button'>Trending</Link>
