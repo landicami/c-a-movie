dffc33f660d955cfa3c8cdc2217172e54c086a4b
Camilla Landin <landin.camilla8@gmail.com>
2024-06-18 15:08:14 +0200

    some styling

--- src/pages/AboutPage.tsx
+++ src/pages/AboutPage.tsx
@@ -1,15 +1,17 @@
 const AboutPage = () => {
   return (
-	<div className='cinema-short col-8 d-flex justify-content-center mt-4 p-4 border'>
+	<div className='bg-white text-dark col-8 d-flex justify-content-center mt-4 p-4 border rounded'>
 		<div className='row'>
 			<div>
 				<h1>Hi, this is my schoolproject</h1>
 			</div>
 			<div>
 				<p>
-					Im using the api from <a href="https://developer.themoviedb.org/">here</a> with bearer key.
-					I only use 500 results in pagination due to <a href="https://www.themoviedb.org/talk/61bbb4dc6a300b00977d906c">this info	</a>
+					Im using the api from <a className="movie-link" href="https://developer.themoviedb.org/">here</a> with bearer key.
+					I only use 500 results in pagination due to <a className="movie-link" href="https://www.themoviedb.org/talk/61bbb4dc6a300b00977d906c">this info</a>.
 				</p>
+					<hr/>
+					<a className="movie-link" href="https://github.com/landicami">Visit my github! </a>
 			</div>
 		</div>
 	</div>
